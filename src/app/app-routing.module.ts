20406b730033ea9536354a65fdbdd609cf93cf61
jassem-1 <soueyjassem@gmail.com>
2024-11-10 05:46:46 -0800

    all crud implimented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListProviderComponent } from './list-provider/list-provider.component';
+import { UpdateProviderComponent } from './update-provider/update-provider.component';
+import { AddProviderComponent } from './add-provider/add-provider.component';
+import { ListArticleComponent } from './list-article/list-article.component'; // Import your article list component
+import { AddArticleComponent } from './add-article/add-article.component'; // Import your add article component
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'app-navbar' },
+  { path: 'listProvider', component: ListProviderComponent },
+  { path: 'addProvider', component: AddProviderComponent },
+  { path: 'updateProvider/:id', component: UpdateProviderComponent },
+  { path: 'listArticle', component: ListArticleComponent }, // Route for articles list
+  { path: 'addArticle', component: AddArticleComponent }, // Route for adding an article
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { UpdateProviderComponent } from './update-provider/update-provider.component';
+import { ListProviderComponent } from './list-provider/list-provider.component';
+import { AddProviderComponent } from './add-provider/add-provider.component';
+import { HttpClientJsonpModule, HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ListArticleComponent } from './list-article/list-article.component';
+import { AddArticleComponent } from './add-article/add-article.component';
+import { ArticleService } from './services/article.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    UpdateProviderComponent,
+    ListProviderComponent,
+    AddProviderComponent,
+    ListArticleComponent,
+    AddArticleComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+
   ],
-  providers: [],
+  providers: [ArticleService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
